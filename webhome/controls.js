1f81a31b48fb67a0eba256cd5d32bc6989247592
Brian Di Palma <offler@gmail.com>
2013-04-21 18:27:27 +0100

    Update to use later version of API.

--- webhome/controls.js
+++ webhome/controls.js
@@ -13,6 +13,7 @@ function init(){
 
 function sendControl(direction){
 	var xhr = new XMLHttpRequest();
-	xhr.open('POST', '/control/' + direction);
+//	xhr.open('POST', '/control?' + direction + '/10');
+	xhr.open('POST', '/control?move=0.4&steer=-0.1');
 	xhr.send();
 }
